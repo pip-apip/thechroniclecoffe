a34253ecb702c590154f2e12b1020c2aafd61fef
Baguser7 <bagushendrawan01@gmail.com>
2025-02-04 22:58:11 +0700

    fe: add menu

--- Frontend/javascript/script.js
+++ Frontend/javascript/script.js
@@ -192,6 +192,46 @@ function renderBlogData2(data) {
 }
 
 // Ensure the DOM is fully loaded before running the function
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   renderTemplates();
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/photos?_limit=8"
+  );
+  const data = await response.json();
+  renderCatalogueMenu(data);
+  renderCatalogueMenuSM(data);
 });
+
+function renderCatalogueMenu(data) {
+  const specificIndex = 2; // Change this to the desired index
+  if (specificIndex >= 0 && specificIndex + 1 < data.length) {
+    const specificData = data;
+    loadTemplate("./javascript/template/catalogue-page.hbs").then(
+      (templateSource) => {
+        const template = Handlebars.compile(templateSource);
+        const html = template(specificData);
+        // console.log(html);
+        document.getElementById("catalogue-content_2").innerHTML = html;
+      }
+    );
+  } else {
+    console.error("Index out of bounds for catalogue");
+  }
+}
+
+function renderCatalogueMenuSM(data) {
+  const specificIndex = 2; // Change this to the desired index
+  if (specificIndex >= 0 && specificIndex + 1 < data.length) {
+    const specificData = data;
+    loadTemplate("./javascript/template/catalogue-page.hbs").then(
+      (templateSource) => {
+        const template = Handlebars.compile(templateSource);
+        const html = template(specificData);
+        // console.log(html);
+        document.getElementById("catalogue-content").innerHTML = html;
+      }
+    );
+  } else {
+    console.error("Index out of bounds for catalogue");
+  }
+}
